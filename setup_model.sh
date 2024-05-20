e4edcc9f6e22b974db2d7e5c1ddf42871692481e
narain1 <narainp@outlook.com>
2024-05-19 17:21:56 -0700

    updated setup model

--- setup_model.sh
+++ setup_model.sh
@@ -1,6 +1,6 @@
 mkdir models
 curl -L https://huggingface.co/FacebookAI/roberta-base/resolve/main/pytorch_model.bin?download=true -o models/pytorch_model.bin
-curl -L https://huggingface.co/FacebookAI/roberta-base/blob/main/tokenizer.json?download=true -o models/spm.model
-curl -L https://huggingface.co/FacebookAI/roberta-base/blob/main/vocab.json?download=true -o models/config.json
-curl -L https://huggingface.co/FacebookAI/roberta-base/blob/main/config.json?download=true -o models/config.json
-curl -L https://huggingface.co/FacebookAI/roberta-base/blob/main/tokenizer_config.json?download=true -o models/config.json
+curl -L https://huggingface.co/FacebookAI/roberta-base/resolve/main/tokenizer.json?download=true -o models/tokenizer.json
+curl -L https://huggingface.co/FacebookAI/roberta-base/resolve/main/vocab.json?download=true -o models/vocab.json
+curl -L https://huggingface.co/FacebookAI/roberta-base/resolve/main/config.json?download=true -o models/config.json
+curl -L https://huggingface.co/FacebookAI/roberta-base/resolve/main/tokenizer_config.json?download=true -o models/tokenizer_config.json
